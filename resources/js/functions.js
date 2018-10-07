1167f83f0dc2b70151591699f28c241b39222936
jocafamaka <joao_carlos.r@hotmail.com>
2018-10-07 02:19:11 -0300

    Improved user experience | Compatibility fixes
    The following changes were made:
    - Several visual changes for a better user experience.
    - Changes to better compatibility with Firefox.
    - Adaptations for slow connections, avoiding errors.

--- resources/js/functions.js
+++ resources/js/functions.js
@@ -5,17 +5,17 @@
   * See the project: https://github.com/jocafamaka/nagmapReborn
   */
 
-function addLines(){
-	for(host = 0 ; host < hostStatus.length ; host++){
-		if(Array.isArray(hostStatus[host].parents)){
-			for (var i = hostStatus[host].parents.length - 1; i >= 0; i--) {
-				for (var ii = LINES.length - 1; ii >= 0; ii--) {
-					if( (hostStatus[host].host_name == LINES[ii].host) && (hostStatus[host].parents[i] == LINES[ii].parent))
-						LINES[ii].line.setMap(window.map);
-				}          
-			}
-		}
-	}
+  function addLines(){
+   for(host = 0 ; host < hostStatus.length ; host++){
+    if(Array.isArray(hostStatus[host].parents)){
+     for (var i = hostStatus[host].parents.length - 1; i >= 0; i--) {
+      for (var ii = LINES.length - 1; ii >= 0; ii--) {
+       if( (hostStatus[host].host_name == LINES[ii].host) && (hostStatus[host].parents[i] == LINES[ii].parent))
+        LINES[ii].line.setMap(window.map);
+    }          
+  }
+}
+}
 }
 
 function removeLines(){
@@ -63,56 +63,31 @@ function changeIconStyle(style){
 
 function randomStatus(){
   hosts = {
-    "Host_3": {
-      "status": (Math.floor(Math.random() * 11) > 7)? Math.floor(Math.random() * 5) : 0,
-      "time": "(elapsed time)"
-    },
-    "Host_1": {
-      "status": (Math.floor(Math.random() * 11) > 7)? Math.floor(Math.random() * 5) : 0,
-      "time": "(elapsed time)"
-    },
-    "Host_2": {
-      "status": (Math.floor(Math.random() * 11) > 7)? Math.floor(Math.random() * 5) : 0,
-      "time": "(elapsed time)"
-    },
-    "Host_10": {
-      "status": (Math.floor(Math.random() * 11) > 7)? Math.floor(Math.random() * 5) : 0,
-      "time": "(elapsed time)"
-    },
-    "Host_8": {
-      "status": (Math.floor(Math.random() * 11) > 7)? Math.floor(Math.random() * 5) : 0,
-      "time": "(elapsed time)"
-    },
-    "Host_11": {
-      "status": (Math.floor(Math.random() * 11) > 7)? Math.floor(Math.random() * 5) : 0,
-      "time": "(elapsed time)"
-    },
-    "Host_9": {
-      "status": (Math.floor(Math.random() * 11) > 7)? Math.floor(Math.random() * 5) : 0,
-      "time": "(elapsed time)"
-    },
-    "Host_7": {
-      "status": (Math.floor(Math.random() * 11) > 7)? Math.floor(Math.random() * 5) : 0,
-      "time": "(elapsed time)"
-    },
-    "Host_4": {
-      "status": (Math.floor(Math.random() * 11) > 7)? Math.floor(Math.random() * 5) : 0,
-      "time": "(elapsed time)"
-    },
-    "Host_6": {
-      "status": (Math.floor(Math.random() * 11) > 7)? Math.floor(Math.random() * 5) : 0,
-      "time": "(elapsed time)"
-    },
-    "Main_Host": {
-      "status": 0,
-      "time": "(elapsed time)"
-    },
-    "Host_5": {
-      "status": (Math.floor(Math.random() * 11) > 7)? Math.floor(Math.random() * 5) : 0,
-      "time": "(elapsed time)"
-    }
+    "Main_Host": {"status": 0, "time": "(elapsed time)"},
+    "Host_1": {"status": -1, "time": "(elapsed time)"},
+    "Host_2": {"status": -1, "time": "(elapsed time)"},
+    "Host_3": {"status": -1, "time": "(elapsed time)"},
+    "Host_4": {"status": -1, "time": "(elapsed time)"},
+    "Host_5": {"status": -1, "time": "(elapsed time)"},
+    "Host_6": {"status": -1, "time": "(elapsed time)"},
+    "Host_7": {"status": -1, "time": "(elapsed time)"},
+    "Host_8": {"status": -1, "time": "(elapsed time)"},
+    "Host_9": {"status": -1, "time": "(elapsed time)"},
+    "Host_10": {"status": -1, "time": "(elapsed time)"},
+    "Host_11": {"status": -1, "time": "(elapsed time)"},
+    "Host_12": {"status": -1, "time": "(elapsed time)"},
+    "Host_13": {"status": -1, "time": "(elapsed time)"}
   };
 
+  for(var i = 0; i < (Math.floor(Math.random() * 6) + 1) ; i++){
+    var chosen = (Math.floor(Math.random() * 14) + 1);
+    var ii = 0;
+    for (var host in hosts){
+      if(ii == chosen)
+        hosts[host].status = Math.floor(Math.random() * 5);
+      ii++;
+    }
+  }
   return hosts;
 };
 
@@ -140,6 +115,78 @@ function GetURLParameter(sParam)
     }
   }
 };
+
+function showMenu(){
+  swal({
+    title: 'MENU',
+    input: 'radio',
+    showCancelButton: true,
+    showConfirmButton: false,
+    inputOptions:{
+      '0': '<img title="Project on GitHub" onclick="window.open(\'https://github.com/jocafamaka/nagmapReborn\'); swal.close();" src="resources/img/ProjectIcon.png">',
+      '1': '<img title="Change" onclick="change();" src="resources/img/RestartIcon.png">'
+    }
+  })
+}
+
+function change(){
+
+  swal.mixin({
+    input: 'radio',
+    confirmButtonText: 'Next &rarr;',
+    showCancelButton: false,
+    progressSteps: ['1', '2', '3']
+  }).queue([
+  {
+    title: 'Select the style of the icons!',
+    inputOptions:{
+      '0': '<img title="Retro" src="resources/img/icons/select/IconsStyle-1.png">',
+      '1': '<img title="With shadow" src="resources/img/icons/select/IconsStyle-2.png">',
+      '2': '<img title="With border" src="resources/img/icons/select/IconsStyle-3.png">',
+    },
+    inputValidator: (value) => {
+      return !value && 'You need to choose something!'
+    }
+  },
+  {
+    title: 'Show line between parents?',
+    inputOptions:{
+      '1': '<img title="On" src="resources/img/lines/on.png">',
+      '3': '<img title="Off" src="resources/img/lines/off.png">',
+    },
+    inputValidator: (value) => {
+      return !value && 'You need to choose something!'
+    }
+  },
+  {
+    title: 'Select map type/style!',
+    inputOptions:{
+      '0': '<img title="Roadmap type" src="resources/img/styles/roadmap.png">',
+      '1': '<img title="Satellite type" src="resources/img/styles/satellite.png"><br>',
+      '2': '<img title="Terrain type" src="resources/img/styles/terrain.png">',
+      '3': '<img title="Dark custom style" src="resources/img/styles/dark.png">',
+    },
+    inputValidator: (value) => {
+      return !value && 'You need to choose something!'
+    },
+    confirmButtonText: 'Finish'
+  }
+  ]).then((result) => {
+    if (result.value) {
+      var r = result.value;
+      changeIconStyle(r[0]);
+      (r[1] == 1) ? addLines() : removeLines();
+      changeStyle(r[2]);
+      swal({
+        type: 'success',
+        title: 'Finished!',
+        html: '<br>All changes applied successfully.',
+        confirmButtonText: 'OK'
+      })
+    }
+  })
+}
+
 
 function onInit(){
 
@@ -179,27 +226,6 @@ function onInit(){
   styledMapTypeDark = new google.maps.StyledMapType([{"elementType":"geometry","stylers":[{"color":"#212121"}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"elementType":"labels.text.stroke","stylers":[{"color":"#212121"}]},{"featureType":"administrative","elementType":"geometry","stylers":[{"color":"#757575"}]},{"featureType":"administrative.country","stylers":[{"visibility":"on"}]},{"featureType":"administrative.country","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"administrative.country","elementType":"labels.text.fill","stylers":[{"lightness":100}]},{"featureType":"administrative.locality","stylers":[{"visibility":"on"}]},{"featureType":"administrative.locality","elementType":"labels","stylers":[{"color":"#ffffff"},{"weight":0.5}]},{"featureType":"administrative.locality","elementType":"labels.icon","stylers":[{"visibility":"on"}]},{"featureType":"administrative.locality","elementType":"labels.text","stylers":[{"color":"#000000"}]},{"featureType":"administrative.locality","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"},{"lightness":100}]},{"featureType":"administrative.locality","elementType":"labels.text.stroke","stylers":[{"color":"#000000"},{"visibility":"on"},{"weight":4}]},{"featureType":"administrative.province","stylers":[{"visibility":"on"}]},{"featureType":"administrative.province","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"on"}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"poi.business","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#181818"}]},{"featureType":"poi.park","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"featureType":"poi.park","elementType":"labels.text.stroke","stylers":[{"color":"#1b1b1b"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#2c2c2c"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#8a8a8a"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#373737"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#3c3c3c"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway.controlled_access","elementType":"geometry","stylers":[{"color":"#4e4e4e"}]},{"featureType":"road.local","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"featureType":"transit","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#3d3d3d"}]}]);
 
   map.mapTypes.set(MY_MAPTYPE_DARK, styledMapTypeDark );
-
-  $('#loader2').css("opacity", "0");
-
-  setTimeout(function(){
-    $('#loader2').hide();
-    swal({
-      type: 'success',
-      title: 'Welcome to NagMap Reborn!',
-      text: 'Due to the limitation of architecture (user-side only) it was not possible to represent some of the features and options of NagMap Reborn, however through this page you can get a working idea of the NagMap Reborn.',
-      footer: 'The data presented here are only demonstrative and generated in a random way, but the occurrences represent the real behavior of the application before the changes of the hosts.',
-      confirmButtonText: 'OK'
-    }); 
-    if(GetURLParameter('icons'))
-      changeIconStyle(GetURLParameter('icons'));
-
-    if(GetURLParameter('lines'))
-      (GetURLParameter('lines') == 1) ? addLines() : removeLines();
-
-    if(GetURLParameter('type'))
-      changeStyle(GetURLParameter('type'));
-  }, 900);
 
   setTimeout(function(){
     swal({
